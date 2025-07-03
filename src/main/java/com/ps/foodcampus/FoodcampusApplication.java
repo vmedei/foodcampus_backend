1d66d317eb3c1d24ca0ae20c16ce83600759c5d4
chrystianruan <chrystianr37@gmail.com>
2025-07-02 22:58:01 -0300

    first commit

---
+++ src/main/java/com/ps/foodcampus/FoodcampusApplication.java
@@ -0,0 +1,13 @@
+package com.ps.foodcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ps/foodcampus/FoodcampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.foodcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
